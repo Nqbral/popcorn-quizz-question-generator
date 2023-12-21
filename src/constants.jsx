66ef2a69bed064a638dc64fc6c31d4daa15c9ce5
Nqbral <sim.chevalier@gmail.com>
2023-12-21 22:40:14 +0100

    Change api URL json server to use from adaptable.io

--- src/constants.jsx
+++ src/constants.jsx
@@ -1,5 +1,5 @@
 export const TOKEN_API_TMDB =
     'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNWQ4ODI1Njc0MmY1ZTZmM2Y2ZjdhNGM3MTg3ZmM0ZiIsInN1YiI6IjY1Njc3YWQwM2Q3NDU0MDE1MDQwNDczNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.j71ItrCqaYiYuhaPKFd7yKYqKOZbpvbiPIavkR-jcxY';
 export const URL_JSON_SERVER =
-    'https://json-server-popcorn-generator.vercel.app/';
+    'https://json-server-popcorn-generator.adaptable.app/';
 // export const URL_JSON_SERVER = 'http://localhost:3000/';
